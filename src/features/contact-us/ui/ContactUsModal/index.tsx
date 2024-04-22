918ee6a443f96866e3e1e74c1bb441691a05be5c
WhtsPoint <whtspoint@gmail.com>
2024-04-22 23:07:31 +0300

    Api link fix; contact us modal change

--- src/features/contact-us/ui/ContactUsModal/index.tsx
+++ src/features/contact-us/ui/ContactUsModal/index.tsx
@@ -11,7 +11,7 @@ interface Params {
     delay?: number
 }
 
-export default function ContactUsModal({ className, delay = 5000 }: Params) {
+export default function ContactUsModal({ className, delay = 30000 }: Params) {
     const [isVisible, setIsVisible] = useState<boolean>(false)
     const [ref, animate] = useContactUsModalAnimation()
     const animateRef = useRef(animate)
