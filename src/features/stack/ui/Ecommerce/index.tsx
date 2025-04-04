8d4de0b4fe67bb2a0c8a1df643d91409b9b9d836
WhtsPoint <whtspoint@gmail.com>
2025-04-05 00:51:24 +0300

    Ecommerce update

--- src/features/stack/ui/Ecommerce/index.tsx
+++ src/features/stack/ui/Ecommerce/index.tsx
@@ -11,14 +11,14 @@ import styles from './styles.module.scss'
 import sectionStyles from '@/utils/assets/styles/stack/section.module.scss'
 import contentStyles from '@/utils/assets/styles/stack/content.module.scss'
 import posterImage from '@/utils/assets/images/stack/posters/ecommerce.jpg'
-import glareImage from '@/utils/assets/images/stack/particles/glare.png'
-import sideLightImage from '@/utils/assets/images/stack/particles/side-light.png'
+import glareImage from '@/utils/assets/images/stack/particles/glare3.png'
+import backGlareImage from '@/utils/assets/images/stack/particles/glare2.png'
 import { useInView } from 'framer-motion'
 import { useRef } from 'react'
 
 const anim = {
-    initial: { opacity: 0, x: 30 },
-    viewAnimation: { opacity: 1, x: 0 },
+    initial: { opacity: 0 },
+    viewAnimation: { opacity: 1 },
 } as const
 
 export default function Ecommerce() {
@@ -31,19 +31,19 @@ export default function Ecommerce() {
         href={config.routes.services.ecommerce}
         className={cl(styles.ecommerce, sectionStyles.section)}
     >
-        <Poster className={styles.poster} src={posterImage.src}>
+        <Poster isInView={isInView} className={styles.poster} src={posterImage.src}>
             <Particle
                 isInView={isInView}
                 className={styles.glare}
                 animation={{ ...anim, transition: { duration: 1, delay: 2 } }}
-                image={{ src: glareImage.src, sizes: '200px' }}
+                image={{ src: glareImage.src, sizes: '25vw' }}
             />
         </Poster>
         <Particle
+            className={styles.backGlare}
+            animation={{ ...anim, transition: { duration: 1, delay: 2 } }}
+            image={{ src: backGlareImage.src, sizes: '50vw' }}
             isInView={isInView}
-            className={styles.sideLight}
-            animation={{ ...anim, transition: { duration: 1, delay: 1 } }}
-            image={{ src: sideLightImage.src, sizes: '300px' }}
         />
         <div className={contentStyles.content}>
             <Title>{t('title')}</Title>
